e8c0c095ec2057bf03ef61b2ada9f0b80617426b
tasyaSukma <maftuhah.tasya@gmail.com>
2020-09-18 13:30:31 +0700

    Update tugas9.js

--- tugas9.js
+++ tugas9.js
@@ -1,6 +1,8 @@
 var dataDiri={firstName:"Tasya Sukma", lastName:"Maftuhah", age:28}
 var x
 
+console.log(dataDiri)
+console.log("Tampilkan menggunakan for in")
 for(x in dataDiri){
-	console.log(dataDiri[x])
+	console.log(x, " : ",dataDiri[x])
 }
