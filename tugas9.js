7b2909ca6763b4a447d28cc3bf4eb97acea003ab
tasyaSukma <maftuhah.tasya@gmail.com>
2020-09-18 13:27:39 +0700

    Update tugas9.js

--- tugas9.js
+++ tugas9.js
@@ -1,16 +1,6 @@
-var angkaGenap = []
-var j = 0;
+var dataDiri={firstName:"Tasya Sukma", lastName:"Maftuhah", age:28}
+var x
 
-console.log("Variabel Objek sebelum pemberian nilai = ",angkaGenap)
-
-for(i=1; i<15; i++){
-	if(i%2 == 0){
-		angkaGenap[j] = i;		
-		console.log("Penambahan pada object ke [",j,"]", " dengan nilai ", i)
-		j++
-	}
-}
-console.log("Setelah penambahan")
-for(i=0; i<angkaGenap.length; i++){
-	console.log("Object ke[",i,"] dengan nilai ", angkaGenap[i])
+for(x in dataDiri){
+	console.log(dataDiri[x])
 }
